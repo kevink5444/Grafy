d5ad72b4952508671c4e0b5b6863b3072d7963d8
kevink5444 <kevinkurniawan901@gmail.com>
2023-04-27 09:13:54 +0700

    navbar

--- app/src/main/java/com/kelompok4/grafy/MainActivity.kt
+++ app/src/main/java/com/kelompok4/grafy/MainActivity.kt
@@ -7,25 +7,18 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var button1 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        button1 = findViewById(R.id.button1)
-        button1.setOnClickListener(this)
-
-
     }
 
-    override fun onClick(v: View) {
-        when(v.id){
-            R.id.button1 ->{
-                val intent = Intent(this@MainActivity,Settings::class.java)
-                startActivity(intent)
-            }
-        }
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
-
-
 }
+
+
+
+
+
